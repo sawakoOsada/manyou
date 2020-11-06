740bf47ac327553aede24500a74d09ad4b4df8a4
sawako osada <mugiwaranana@yahoo.co.jp>
2020-11-06 16:20:15 +0000

    Add:task詳細画面を作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,6 +12,10 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def show
+    @task = Task.find(params[:id])
+  end
+
   private
   def task_params
     params.require(:task).permit(:name, :content)
