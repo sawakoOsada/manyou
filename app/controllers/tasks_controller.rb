252e158b09c0f91f07ed7c208fbc63ba39ecbef8
sawako osada <mugiwaranana@yahoo.co.jp>
2020-11-06 16:36:04 +0000

    Add:task編集機能を作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,6 +16,19 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      render :edit
+    end
+  end
+
   private
   def task_params
     params.require(:task).permit(:name, :content)
