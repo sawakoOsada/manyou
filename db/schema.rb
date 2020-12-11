8c608377bcad320755523ef5e9d0701eb484700c
sawako osada <mugiwaranana@yahoo.co.jp>
2020-12-11 12:56:52 +0000

    Add:nameカラムに検索インデックスを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_08_104351) do
+ActiveRecord::Schema.define(version: 2020_12_11_125507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_12_08_104351) do
     t.datetime "deadline", null: false
     t.integer "state", default: 0, null: false
     t.integer "priority", default: 0, null: false
+    t.index ["name"], name: "index_tasks_on_name"
   end
 
 end
