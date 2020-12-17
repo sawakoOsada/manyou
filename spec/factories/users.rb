872ef0f4fdd909fe377af813fc2f1fcb5a76f17b
sawako osada <mugiwaranana@yahoo.co.jp>
2020-12-17 17:09:46 +0000

    Add:UserSystemSpec管理者テスト作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,10 +3,12 @@ FactoryBot.define do
     name { "test_user" }
     sequence(:email) { |n| "user#{n}@test.com" }
     password { "test_password" }
+    admin { false }
   end
-  factory :admin do
+  factory :admin, class: User do
     name { "admin" }
     sequence(:email) { |n| "admin#{n}@test.com" }
     password { "admin_password" }
+    admin { true }
   end
 end
--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 RSpec.describe 'ログイン機能', type: :system do
-  let!(:user) {FactoryBot.create(:user, email: 'aaaa@aaaa.com', password: 'aaaaaa')}
+  let!(:admin) {FactoryBot.create(:admin, email: 'admin@aaaa.com', password: 'aaaaaa')}
+  let!(:admined) {FactoryBot.create(:user, name: 'admined', email: 'admined@aaaa.com', password: 'aaaaaa')}
 
   describe '新規登録機能' do
+
     context 'ユーザーを新規登録した場合' do
       it 'ユーザーのプロフィールが表示される' do
         visit new_user_path
@@ -14,6 +16,7 @@ RSpec.describe 'ログイン機能', type: :system do
         expect(page).to have_content 'new_userのページ'
       end
     end
+
     context '未ログイン状態でタスク一覧画面へ飛ぼうとした場合' do
       it 'ログイン画面に遷移する' do
         get tasks_url
@@ -25,24 +28,24 @@ RSpec.describe 'ログイン機能', type: :system do
   describe 'セッション機能' do
     context 'ユーザーが登録されている場合' do
       it 'ログインができる' do
-        signed_user = FactoryBot.create(:user, name: 'signed_user', email: 'sign@aaaa.com', password: 'aaaaaa')
         visit new_session_path
-        fill_in 'メールアドレス', with: 'sign@aaaa.com'
+        fill_in 'メールアドレス', with: 'admined@aaaa.com'
         fill_in 'パスワード', with: 'aaaaaa'
         click_on 'Log in'
-        expect(page).to have_content 'signed_userのページ'
+        expect(page).to have_content 'adminedのページ'
       end
     end
+
     context 'ログインしている場合' do
       before do
         visit root_path
-        fill_in 'メールアドレス', with: 'aaaa@aaaa.com'
+        fill_in 'メールアドレス', with: 'admined@aaaa.com'
         fill_in 'パスワード', with: 'aaaaaa'
         click_on 'Log in'
       end
       it '自分の詳細画面に飛べる' do
-        visit user_path(user.id)
-        expect(page).to have_content 'test_userのページ'
+        visit user_path(admined.id)
+        expect(page).to have_content 'adminedのページ'
       end
       it '他人の詳細画面に飛ぶとタスク一覧画面に遷移する' do
         another_user = FactoryBot.create(:user, name: 'another_user', email: 'another@aaaa.com', password: 'aaaaaa')
@@ -52,6 +55,63 @@ RSpec.describe 'ログイン機能', type: :system do
       it 'ログアウトができる' do
         find('.logout_icon').click
         expect(page).to have_content 'ログアウトしました'
+      end
+    end
+  end
+
+  describe '管理機能' do
+
+    context '一般ユーザーの場合' do
+      it '管理画面へアクセスするとタスク一覧ページへ飛ぶ' do
+        visit root_path
+        fill_in 'メールアドレス', with: 'admined@aaaa.com'
+        fill_in 'パスワード', with: 'aaaaaa'
+        click_on 'Log in'
+        get admin_users_url
+        expect(page).to redirect_to( tasks_url )
+      end
+    end
+
+    context '管理ユーザーの場合' do
+
+      before do
+        visit root_path
+        fill_in 'メールアドレス', with: 'admin@aaaa.com'
+        fill_in 'パスワード', with: 'aaaaaa'
+        click_on 'Log in'
+      end
+
+      it '管理画面にアクセスできる' do
+        binding.pry
+        get admin_users_url
+        expect(page).to redirect_to(admin_users_url)
+      end
+      it 'ユーザーの新規登録ができる' do
+        visit new_admin_user_path
+        fill_in '名前', with: 'test_user'
+        fill_in 'メールアドレス', with: 'test@example.com'
+        fill_in 'パスワード', with: 'test_password'
+        fill_in 'パスワードの確認', with: 'test_password'
+        click_on 'Create account'
+        expect(page).to have_content 'ユーザーを作成しました'
+      end
+      it 'ユーザーの詳細画面にアクセスできる' do
+        get admin_users_url(admined.id)
+        expect(page).to redirect_to(admin_users_url(admined.id))
+      end
+      it 'ユーザーの編集画面からユーザーを編集できる' do
+        visit edit_admin_user_path(admined.id)
+        fill_in '名前', with: 'edited_user'
+        fill_in 'メールアドレス', with: 'edited@example.com'
+        choose '管理者'
+        click_on '変更する'
+        expect(page).to have_content 'ユーザーを編集しました'
+      end
+      it 'ユーザーの削除ができる' do
+        visit admin_users_path
+        first(".delete_button").click
+        page.driver.browser.switch_to.alert.accept
+        expect(page).to have_content 'ユーザーを削除しました'
       end
     end
   end
